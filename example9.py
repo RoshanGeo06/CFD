67af0d3dddd16845c07be0d2427a7ba0d3491649
RoshanGeo06 <112683381+RoshanGeo06@users.noreply.github.com>
2023-05-29 22:12:39 +0530

    Add files via upload

---
+++ example9.py
@@ -0,0 +1,199 @@
+import argparse
+import numpy as np
+import meshio
+import matplotlib.pyplot as plt
+
+
+class Mesh:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = None
+        self.strip_points = None
+        self.wave_displacements = None
+        self.vertices = None
+
+    def load(self):
+        try:
+            with open(self.file_path, 'r', encoding='latin-1') as file:
+                self.data = file.read()
+        except FileNotFoundError:
+            print(f"Error: Mesh file '{self.file_path}' not found.")
+            return False
+        return True
+
+    def process(self):
+        self.calculate_strip_points(self.file_path)
+        self.calculate_wave_displacements()
+        self.find_vertices()
+
+    def calculate_strip_points(self, mesh_file):
+        # Load the ANSYS mesh file
+        mesh = meshio.read(mesh_file)
+        vertices = mesh.points
+        elements = mesh.cells[0].data
+
+        # Implement strip point calculation logic
+        # Calculate the centroid of each element
+        centroids = []
+        for element in elements:
+            element_vertices = vertices[element]
+            centroid = np.mean(element_vertices, axis=0)
+            centroids.append(centroid)
+
+        # Convert centroids to numpy array
+        strip_points = np.array(centroids)
+
+        # Store the calculated strip points
+        self.strip_points = strip_points
+
+    def calculate_wave_displacements(self):
+        if self.strip_points is None:
+            print("Error: Strip points not found. Please calculate strip points first.")
+            return
+
+        # Define wave properties
+        wave_amplitude = 0.1
+        wave_frequency = 2
+        wave_speed = 1
+
+        # Calculate wave displacements for each strip point
+        wave_displacements = []
+        for point in self.strip_points:
+            distance = np.linalg.norm(point)
+            phase = 2 * np.pi * distance * wave_frequency / wave_speed
+            displacement = wave_amplitude * np.array([np.sin(phase), np.cos(phase)])
+            wave_displacements.append(displacement)
+
+        # Convert the wave displacements list to a numpy array
+        self.wave_displacements = np.array(wave_displacements)
+
+    def find_vertices(self):
+        if self.data is None:
+            print("Error: Mesh data not loaded. Please load the mesh data first.")
+            return
+
+        # Load the mesh using meshio
+        mesh = meshio.read(self.file_path)
+
+        # Extract the vertices from the mesh
+        vertices = mesh.points
+
+        # Store the vertices
+        self.vertices = vertices
+
+
+class Simulation:
+    def __init__(self, mesh_file, surface_vessel, inflow_conditions="turbulent", outflow_conditions="open",
+                 free_surface_conditions="fixed", hull_boundary_conditions="rough"):
+        self.mesh_file = mesh_file
+        self.surface_vessel = surface_vessel
+        self.inflow_conditions = inflow_conditions
+        self.outflow_conditions = outflow_conditions
+        self.free_surface_conditions = free_surface_conditions
+        self.hull_boundary_conditions = hull_boundary_conditions
+
+    def run(self):
+        mesh = Mesh(self.mesh_file)
+        if not mesh.load():
+            return
+
+        mesh.process()
+
+        simulation_environment = {
+            'mesh': mesh.data,
+            'surface_vessel': self.surface_vessel,
+            'boundary_conditions': {
+                'inflow': self.inflow_conditions,
+                'outflow': self.outflow_conditions,
+                'free_surface': self.free_surface_conditions,
+                'hull_boundary': self.hull_boundary_conditions
+            }
+        }
+
+        simulation_environment['strip_points'] = mesh.strip_points
+        simulation_environment['wave_displacements'] = mesh.wave_displacements
+        simulation_environment['vertices'] = mesh.vertices
+
+        self.display_simulation_results(simulation_environment)
+
+        self.run_cfd_simulation(simulation_environment)
+
+    def display_simulation_results(self, simulation_environment):
+        # Display simulation results
+        print("Simulation Results:")
+
+        strip_points = simulation_environment['strip_points']
+        wave_displacements = simulation_environment['wave_displacements']
+        vertices = simulation_environment['vertices']
+
+        # Plot strip points
+        plt.figure(figsize=(8, 6))
+        plt.scatter(strip_points[:, 0], strip_points[:, 1], c='blue')
+        plt.title("Strip Points")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.grid(True)
+        plt.show()
+
+        # Plot wave displacements
+        plt.figure(figsize=(8, 6))
+        plt.quiver(strip_points[:, 0], strip_points[:, 1],
+                   wave_displacements[:, 0], wave_displacements[:, 1])
+        plt.title("Wave Displacements")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.grid(True)
+        plt.show()
+
+        # Plot vertices
+        fig = plt.figure(figsize=(8, 6))
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(vertices[:, 0], vertices[:, 1], vertices[:, 2], c='red')
+        ax.set_title("Vertices")
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_zlabel("Z")
+        plt.show()
+
+
+    def run_cfd_simulation(self, simulation_environment):
+        # Extract simulation environment variables
+        #mesh_data = simulation_environment['mesh']
+        surface_vessel = simulation_environment['surface_vessel']
+        boundary_conditions = simulation_environment['boundary_conditions']
+        strip_points = simulation_environment['strip_points']
+        wave_displacements = simulation_environment['wave_displacements']
+        vertices = simulation_environment['vertices']
+
+        # Perform the CFD simulation using the extracted variables
+        print("Running CFD simulation...")
+        # Your CFD simulation code here
+
+        # For demonstration purposes, let's print the extracted variables
+        #print("Mesh Data:", mesh_data)
+        print("Surface Vessel:", surface_vessel)
+        print("Boundary Conditions:", boundary_conditions)
+        print("Strip Points:", strip_points)
+        print("Number of Strip Points:", len(strip_points))
+        print()
+        print("Wave Displacements:", wave_displacements)
+        print("Number of Wave Displacements:", len(wave_displacements))
+        print()
+        print("Vertices:", vertices)
+        print("Number of Vertices:", len(vertices))
+        print()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Surface Vessel CFD Simulation Tool")
+    parser.add_argument("mesh_file", help="Path to the mesh file")
+    parser.add_argument("surface_vessel", help="Surface vessel name")
+    parser.add_argument("--inflow", default="turbulent", help="Inflow conditions")
+    parser.add_argument("--outflow", default="open", help="Outflow conditions")
+    parser.add_argument("--free_surface", default="fixed", help="Free surface conditions")
+    parser.add_argument("--hull_boundary", default="rough", help="Hull boundary conditions")
+    args = parser.parse_args()
+
+    simulation = Simulation(args.mesh_file, args.surface_vessel, args.inflow,
+                            args.outflow, args.free_surface, args.hull_boundary)
+    simulation.run()
